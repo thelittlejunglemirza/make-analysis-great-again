cd783449a712a0f543ff749a5534b15c9bb348eb
hajarash <hajarash@students.cs.ubc.ca>
2019-11-17 13:46:48 -0800

    Initial additions for github API calls

--- data_collection/github_client.py
+++ data_collection/github_client.py
@@ -1,13 +1,38 @@
+import requests
+import json
 class GitHubClient:
+    def __init__(self, username, repo):
+        self.username = username
+        self.repo = repo
 
-    def __init__(self, public_repo_link):
-        self.repo_link = public_repo_link
+    def get_repo_info(self):
+        #GET /repos/:owner/:repo/
+        # repository has to be public
+        username = 'django'
+        repository = 'django'
+        r = requests.get('https://api.github.com/repos/{}/{}'.format(self.username, self.repository))
+        if (r.ok):
+            print(r.json())
+            return r.json()
 
-    def get_repo_info(self, repo_link):
         pass
 
     def get_collaborators(self):
+        #GET /repos/:owner/:repo/contributors
+        r = requests.get('https://api.github.com/repos/{}/{}/contributors'.format(self.username, self.repository))
+
+        if (r.ok):
+            print(r.json())
+            return r.json()
+
         pass
 
     def get_commits(self):
+
+        r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(self.username, self.repository))
+        if (r.ok):
+            print(r.json())
+            return r.json()
+
+        #GET / repos /: owner /:repo / commits
         pass
