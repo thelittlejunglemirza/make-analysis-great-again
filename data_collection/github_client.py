e8595555cba37aef9a49b1f28cd8d46a07202172
masoudjaveri <masoudjaveri@yahoo.com>
2019-11-17 16:52:52 -0800

    refactors GitHubClient
    adds script to get all commits

--- data_collection/github_client.py
+++ data_collection/github_client.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+
+GITHUB_API_BASE_URI = 'https://api.github.com/repos/'
+
 class GitHubClient:
     def __init__(self, username, repo):
         self.username = username
@@ -8,25 +11,17 @@ class GitHubClient:
 
     def get_repo_info(self):
         # repository has to be public
-        r = requests.get('https://api.github.com/repos/{}/{}'.format(self.username, self.repo))
+        r = requests.get('{}{}/{}'.format(GITHUB_API_BASE_URI, self.username, self.repo))
         if (r.ok):
-            print(r.json())
             return r.json()
-
-        pass
 
     def get_collaborators(self):
-        r = requests.get('https://api.github.com/repos/{}/{}/contributors'.format(self.username, self.repo))
+        r = requests.get('{}{}/{}/contributors'.format(GITHUB_API_BASE_URI, self.username, self.repo))
         if (r.ok):
-            print(r.json())
-            return r.json()
+            return json.loads(r.content)
 
-        pass
 
-    def get_commits(self):
-
-        r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(self.username, self.repo))
+    def get_commits(self, page=1):
+        r = requests.get('{}{}/{}/commits?page={}'.format(GITHUB_API_BASE_URI, self.username, self.repo, page))
         if (r.ok):
-            print(r.json())
             return r.json()
-        pass
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from data_collection.github_client import GitHubClient
+import json
 
 username = 'django'
 repository = 'django'
 client = GitHubClient(username, repository)
 
-client.get_collaborators()
+
+# json.dumps()
+commit_list = []
+
+recent_result = []
+
+page = 1
+while True:
+    print(page)
+    recent_result = client.get_commits(page)
+    page = page + 1
+    if len(recent_result) == 0:
+        break
+    commit_list.extend(recent_result)
+
+
+print(commit_list)
