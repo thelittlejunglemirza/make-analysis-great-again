3fbf294f8ff4c7e6d673bb173fe57b2169610b04
masoudjaveri <masoudjaveri@yahoo.com>
2019-11-23 18:46:47 -0800

    adds script to get all edges and nodes for data visualization

--- data_visualization/structures.py
+++ data_visualization/structures.py
@@ -2,11 +2,25 @@ from .constants import NodeType
 
 
 class Node:
-    type: NodeType
-    name: str
+    def __init__(self, type: NodeType, name: str):
+        self.type = type
+        self.name = name
 
+    def __str__(self):
+        return str(self.type) + ' ' + self.name
+
+    @classmethod
+    def get_node_by_name(cls, nodes, search_name):
+        li = list(filter(lambda node: node.name == search_name, nodes))
+        if len(li) == 1:
+            return li[0]
+        return None
 
 class Edge:
-    src: Node
-    dst: Node
-    weight: int
+
+    def __init__(self, src: Node, dst: Node, weight:int):
+        self.src = src
+        self.dst = dst
+        self.weight = weight
+
+
