8892bd741b505d73094a7cc0d5070dd6cb591fcf
Nader Samadyan <nader.smdn@gmail.com>
2019-11-25 19:13:32 -0800

    added main command arguments

--- data_visualization/boxplot.py
+++ data_visualization/boxplot.py
@@ -1,11 +1,13 @@
 import plotly.graph_objects as go
-from data_collection.processes.module_name_to_commit_dates import get_module_name_to_commit_dates
+from data_collection.processes.box_plot_data import get_module_name_to_commit_dates
 
-module_name_to_dates_map = get_module_name_to_commit_dates()
-fig = go.Figure()
 
-for module_name in module_name_to_dates_map.keys():
-    dates = module_name_to_dates_map[module_name]
-    fig.add_trace(go.Box(y=dates, name=module_name))
+def plot_boxplot():
+    module_name_to_dates_map = get_module_name_to_commit_dates()
+    fig = go.Figure()
 
-fig.show()
+    for module_name in module_name_to_dates_map.keys():
+        dates = module_name_to_dates_map[module_name]
+        fig.add_trace(go.Box(y=dates, name=module_name))
+
+    fig.show()
